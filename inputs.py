1d95de641a629a841943eb6e327ac6eff0ff5807
miguelmcunha <miguelmcunha@tecnico.ulisboa.pt>
2024-08-30 04:16:55 +0100

    Inputs for simple stuff. Going to make specific ones for each airliner.

---
+++ inputs.py
@@ -0,0 +1,23 @@
+input_print_ = 3
+# TAP handels 5. Ryanair 10. Easyjet is okay with 10. 30 for Swiss
+input_timeout = 30
+# 5 for easyjet. 1.5 for Ryanair and TAP
+input_timeout_cookies = 1.5
+# Swiss has a 0.2 timeout_little. Consider adding micro. placing at 5 for iberia
+input_timeout_retry_click = 1.5
+input_timeout_little = 5
+input_timeout_checks = 4
+input_timeout_implicitly_wait = 5
+input_cookies = "not accepted"
+
+swiss_retries = 3
+
+# JUST LOGGING THIS FOR NOW
+iberia_print_ = 3
+iberia_timeout = 30
+iberia_timeout_cookies = 1.5
+iberia_timeout_retry_click = 1.5
+iberia_timeout_little = 5
+iberia_timeout_checks = 4
+iberia_timeout_implicitly_wait = 5
+iberia_cookies = "not accepted"
