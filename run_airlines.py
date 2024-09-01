42cd82d565b0bb9d1df32ea53859f7b6991546a4
miguelmcunha <miguelmcunha@tecnico.ulisboa.pt>
2024-09-01 01:59:13 +0100

    Update the file policies

--- run_airlines.py
+++ run_airlines.py
@@ -28,6 +28,8 @@ if __name__ == "__main__":
     airlines_file_name = args.airlines_file
 
     log_filenames = []
+    outputs_filenames_to_upload = []
+    outputs_filenames_to_delete = []
 
     with open(airlines_file_name, 'r') as f:
 
@@ -39,25 +41,43 @@ if __name__ == "__main__":
 
                 airline_start_time = time.time()
 
+                now = datetime.datetime.now()
+                yesterday = now - datetime.timedelta(days=1)
+                two_days_ago = now - datetime.timedelta(days=2)
+
+                yesterday_file_name = airline + '/' + 'outputs' + '/' + airline + '_' + yesterday.strftime('%d-%m-%Y') + '.csv'
+                outputs_filenames_to_upload.append(yesterday_file_name)
+                yesterday_file_name_economy = airline + '/' + 'outputs' + '/' + airline + '_' + yesterday.strftime('%d-%m-%Y') + '_' + 'Economy' + '.csv'
+                outputs_filenames_to_upload.append(yesterday_file_name_economy)
+                yesterday_file_name_business = airline + '/' + 'outputs' + '/' + airline + '_' + yesterday.strftime('%d-%m-%Y') + '_' + 'Business' + '.csv'
+                outputs_filenames_to_upload.append(yesterday_file_name_business)
+
+                two_days_ago_file_name = airline + '/' + 'outputs' + '/' + airline + '_' + two_days_ago.strftime('%d-%m-%Y') + '.csv'
+                outputs_filenames_to_delete.append(two_days_ago_file_name)
+                two_days_ago_file_name_economy = airline + '/' + 'outputs' + '/' + airline + '_' + two_days_ago.strftime('%d-%m-%Y') + '_' + 'Economy' + '.csv'
+                outputs_filenames_to_delete.append(two_days_ago_file_name_economy)
+                two_days_ago_file_name_business = airline + '/' + 'outputs' + '/' + airline + '_' + two_days_ago.strftime('%d-%m-%Y') + '_' + 'Business' + '.csv'
+                outputs_filenames_to_delete.append(two_days_ago_file_name_business)
+
                 if not os.path.isdir(airline):
-                    if inputs.input_print_ > 0:
+                    if inputs.input_print_ > -1:
                         print(f'Creating directory for {airline}')
                     os.mkdir(airline)
 
                 if not os.path.isdir(airline + '/' + 'logs'):
-                    if inputs.input_print_ > 0:
+                    if inputs.input_print_ > -1:
                         print(f'Creating logs directory for {airline}')
                     os.mkdir(airline + '/' + 'logs')
 
                 if not os.path.isdir(airline + '/' + 'outputs'):
-                    if inputs.input_print_ > 0:
+                    if inputs.input_print_ > -1:
                         print(f'Creating outputs directory for {airline}')
                     os.mkdir(airline + '/' + 'outputs')
                 
                 filename = airline + '_' + 'routes' + '.csv'
                 
                 df = pd.read_csv(filename)
-                if inputs.input_print_ > 0:
+                if inputs.input_print_ > -1:
                     print(f'Number of routes found for {airline}: {len(df)}')
                 for index, row in df.iterrows():
                     origin_code = row['origin_code']
@@ -73,7 +93,7 @@ if __name__ == "__main__":
 
                     log_filenames.append(log_filename)
 
-                    if inputs.input_print_ > 0:
+                    if inputs.input_print_ > -1:
                         print(f'Running main function for {airline} for index: {row_index}')
 
                     with open(log_filename, 'w') as log_file:
@@ -81,41 +101,106 @@ if __name__ == "__main__":
                                     stdout=log_file, 
                                     stderr=subprocess.STDOUT)
                     
-                    if inputs.input_print_ > 0:
+                    if inputs.input_print_ > -1:
                         print(f'{airline} route from {origin_name} to {destination_name} on {date} was run')
                 
                 airline_end_time = time.time()
                 airline_time = airline_end_time - airline_start_time
-                if inputs.input_print_ > 0:
+                if inputs.input_print_ > -1:
                     print(f'Time taken for {airline}: {str(datetime.timedelta(seconds=airline_time))}')
         
             else:
-                if inputs.input_print_ > 0:
+                if inputs.input_print_ > -1:
                     print(f'Airline {airline} not found')
                 continue
 
         end_time = time.time()
         total_time = end_time - start_time
-        if inputs.input_print_ > 0:
+        if inputs.input_print_ > -1:
             print(f'Total time taken: {str(datetime.timedelta(seconds=total_time))}')
 
 
-    # Send log files to S3.
-    # Delete all the logs after sending them to S3.
-    for log_file in log_filenames:
-        if inputs.input_print_ > 0:
-            print(f'Sending log file {log_file} to S3')
-        s3_client = boto3.client('s3')
+        # Send log files to S3.
+        # Delete all the logs after sending them to S3.
+        for log_file in log_filenames:
+            if inputs.input_print_ > -1:
+                print(f'Sending log file {log_file} to S3')
+            s3_client = boto3.client('s3')
+
+            # if file exists, send it to S3
+            if os.path.isfile(log_file):
+                try:
+                    s3_client.upload_file(log_file, 'airline_scrapper_files', os.path.basename(log_file))
+                    if inputs.input_print_ > -1:
+                        print(f'Log file {log_file} sent to S3')
+                    
+                    os.remove(log_file)
+                    if inputs.input_print_ > -1:
+                        print(f'Log file {log_file} deleted')
+                except Exception as e:
+                    if inputs.input_print_ > -1:
+                        print(f'Error sending log file {log_file} to S3: {str(e)}')
+                    continue
+            else:
+                if inputs.input_print_ > -1:
+                    print(f'Log file {log_file} does not exist')
+                continue
+
+
+        # Send output files to S3. 
+        #   The files will be in the outputs directory of each airline. 
+        #   Only bring the ones that are dated from yesterday. 
+        #   Delete the ones from 2 days ago.
+        #   Send the ones from yesterday to S3.
+        #   Keep todays files in the outputs directory.
+        for upload_filename in outputs_filenames_to_upload:
+            if inputs.input_print_ > -1:
+                print(f'Sending output file {upload_filename} to S3')
+            s3_client = boto3.client('s3')
+
+            # if file exists, send it to S3
+            if os.path.isfile(upload_filename):
+                try:
+                    s3_client.upload_file(upload_filename, 'airline_scrapper_files', os.path.basename(upload_filename))
+                    if inputs.input_print_ > -1:
+                        print(f'Output file {upload_filename} sent to S3')
+                except Exception as e:
+                    if inputs.input_print_ > -1:
+                        print(f'Error sending output file {upload_filename} to S3: {str(e)}')
+                    continue
+            else:
+                if inputs.input_print_ > -1:
+                    print(f'Output file {upload_filename} does not exist')
+                continue
         
-        os.remove(log_file)
+        for delete_filename in outputs_filenames_to_delete:
+            if inputs.input_print_ > -1:
+                print(f'Deleting output file {delete_filename}')
+            if os.path.isfile(delete_filename):
+                try:
+                    os.remove(delete_filename)
+                    if inputs.input_print_ > -1:
+                        print(f'Output file {delete_filename} deleted')
+                except Exception as e:
+                    if inputs.input_print_ > -1:
+                        print(f'Error deleting output file {delete_filename}: {str(e)}')
+                    continue
+            else:
+                if inputs.input_print_ > -1:
+                    print(f'Output file {delete_filename} does not exist')
+                continue
 
 
-    # Send output files to S3. 
-    #   The files will be in the outputs directory of each airline. 
-    #   Only bring the ones that are dated from yesterday. 
-    #   Delete the ones from 2 days ago.
-    #   Send the ones from yesterday to S3.
-    #   Keep todays files in the outputs directory.
+
+
+    
+
+
+
+        
+
+
+
     
 
     
